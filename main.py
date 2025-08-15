f85de724789afa80350ab144c8bee543aa00f743
Nadine Müller <nadine.selina.mueller@gmail.com>
2025-08-15 14:18:20 +0200

    First few classes, no content yet, just structure

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+# Thing
+class Thing:
+    def __init__(self):
+        pass
+
+## 
+
+class creativeWork(Thing):
+    def __init__(self):
+        super().__init__()
+
+
+class HowTo(creativeWork):
+    def __init__(self):
+        super().__init__()
+
+class Recipe(HowTo):
+    def __init__(self):
+        super().__init__()
+
 
